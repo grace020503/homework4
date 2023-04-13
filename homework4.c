278fea3371436f6cfdbd34062f6f8c51b1ea6751
sieun Kim <grace020503@gmail.com>
2023-04-13 12:27:30 +0900

    주석 1차 추가

--- homework4.c
+++ homework4.c
@@ -1,161 +1,151 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_matrix(int **matrix, int rows, int cols);
-int **allocate_matrix(int rows, int cols);
-void free_matrix(int **matrix, int rows, int cols);
-void addition_matrix(int **matrix_a, int **matrix_b, int rows, int cols);
-void subtraction_matrix(int **matrix_a, int **matrix_b, int rows, int cols);
-void transpose_matrix(int **matrix_a, int rows, int cols);
-void multiply_matrix(int **matrix_a, int **matrix_b, int rows_a, int cols_a, int cols_b);
-
-int main() {
-    int rows_a, cols_a, rows_b, cols_b;
-
-    printf("Enter the number of rows for matrix A: ");
-    scanf("%d", &rows_a);
-    printf("Enter the number of columns for matrix A: ");
-    scanf("%d", &cols_a);
-
-    printf("Enter the number of rows for matrix B: ");
-    scanf("%d", &rows_b);
-    printf("Enter the number of columns for matrix B: ");
-    scanf("%d", &cols_b);
-
-    // Check if matrix A and matrix B can be added, subtracted, or multiplied.
-    if (rows_a != rows_b || cols_a != cols_b) {
-        printf("Error: Matrix A and Matrix B must have the same dimensions.\n");
-        exit(1);
-    }
-    if (cols_a != rows_b) {
-        printf("Error: The number of columns of Matrix A must be equal to the number of rows of Matrix B.\n");
-        exit(1);
-    }
-
-    // Allocate memory for matrix A and matrix B.
-    int **matrix_a = allocate_matrix(rows_a, cols_a);
-    int **matrix_b = allocate_matrix(rows_b, cols_b);
-
-    // Fill matrix A and matrix B with user input.
-    printf("Enter the elements of matrix A:\n");
-    for (int i = 0; i < rows_a; i++) {
-        for (int j = 0; j < cols_a; j++) {
-            scanf("%d", &matrix_a[i][j]);
-        }
-    }
-
-    printf("Enter the elements of matrix B:\n");
-    for (int i = 0; i < rows_b; i++) {
-        for (int j = 0; j < cols_b; j++) {
-            scanf("%d", &matrix_b[i][j]);
-        }
-    }
-
-    // Perform matrix operations.
-    printf("Matrix A:\n");
-    print_matrix(matrix_a, rows_a, cols_a);
-
-    printf("Matrix B:\n");
-    print_matrix(matrix_b, rows_b, cols_b);
-
-    printf("Addition of matrix A and matrix B:\n");
-    addition_matrix(matrix_a, matrix_b, rows_a, cols_a);
-    printf("\n");
-
-    printf("Subtraction of matrix A and matrix B:\n");
-    subtraction_matrix(matrix_a, matrix_b, rows_a, cols_a);
-    printf("\n");
-
-    printf("Transpose of matrix A:\n");
-    transpose_matrix(matrix_a, rows_a, cols_a);
-    printf("\n");
-
-    printf("Multiplication of matrix A and matrix B:\n");
-    multiply_matrix(matrix_a, matrix_b, rows_a, cols_a, cols_b);
-    printf("\n");
-
-    // Free memory used by matrix A and matrix B.
-    free_matrix(matrix_a, rows_a, cols_a);
-    free_matrix(matrix_b, rows_b, cols_b);
-
-    return 0;
-}
-
-void print_matrix(int **matrix, int rows, int cols) {
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+// 행렬 출력 함수
+void printMatrix(int **matrix, int rows, int cols) {
+    printf("Matrix:\n");
+    for(int i=0; i<rows; i++) {
+        for(int j=0; j<cols; j++) {
             printf("%d ", matrix[i][j]);
         }
         printf("\n");
     }
 }
 
-int **allocate_matrix(int rows, int cols) {
+// 행렬 동적 메모리 할당 함수
+int **allocateMatrix(int rows, int cols) {
     int **matrix = (int **)malloc(rows * sizeof(int *));
-    for (int i = 0; i < rows; i++) {
+    for(int i=0; i<rows; i++) {
         matrix[i] = (int *)malloc(cols * sizeof(int));
     }
     return matrix;
 }
 
-void free_matrix(int **matrix, int rows, int cols) {
-    for (int i = 0; i < rows; i++) {
+// 할당된 행렬 동적 메모리 해제 함수
+void freeMatrix(int **matrix, int rows) {
+    for(int i=0; i<rows; i++) {
         free(matrix[i]);
     }
     free(matrix);
 }
 
-void addition_matrix(int **matrix_a, int **matrix_b, int rows, int cols) {
-    int **matrix_c = allocate_matrix(rows, cols);
-
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            matrix_c[i][j] = matrix_a[i][j] + matrix_b[i][j];
+// 행렬 입력 함수
+void inputMatrix(int **matrix, int rows, int cols) {
+    printf("Enter matrix elements:\n");
+    for(int i=0; i<rows; i++) {
+        for(int j=0; j<cols; j++) {
+            scanf("%d", &matrix[i][j]);
         }
     }
-
-    print_matrix(matrix_c, rows, cols);
-    free_matrix(matrix_c, rows, cols);
 }
 
-void subtraction_matrix(int **matrix_a, int **matrix_b, int rows, int cols) {
-    int **matrix_c = allocate_matrix(rows, cols);
-
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            matrix_c[i][j] = matrix_a[i][j] - matrix_b[i][j];
+// 행렬 덧셈 함수
+void addMatrix(int **a, int **b, int **c, int rows, int cols) {
+    for(int i=0; i<rows; i++) {
+        for(int j=0; j<cols; j++) {
+            c[i][j] = a[i][j] + b[i][j];
         }
     }
-
-    print_matrix(matrix_c, rows, cols);
-    free_matrix(matrix_c, rows, cols);
 }
 
-void transpose_matrix(int **matrix_a, int rows, int cols) {
-    int **matrix_c = allocate_matrix(cols, rows);
-
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            matrix_c[j][i] = matrix_a[i][j];
+// 행렬 뺄셈 함수
+void subtractMatrix(int **a, int **b, int **c, int rows, int cols) {
+    for(int i=0; i<rows; i++) {
+        for(int j=0; j<cols; j++) {
+            c[i][j] = a[i][j] - b[i][j];
         }
     }
-
-    print_matrix(matrix_c, cols, rows);
-    free_matrix(matrix_c, cols, rows);
 }
 
-void multiply_matrix(int **matrix_a, int **matrix_b, int rows_a, int cols_a, int cols_b) {
-    int **matrix_c = allocate_matrix(rows_a, cols_b);
+// 행렬 전치 함수
+void transposeMatrix(int **a, int **t, int rows, int cols) {
+    for(int i=0; i<rows; i++) {
+        for(int j=0; j<cols; j++) {
+            t[j][i] = a[i][j];
+        }
+    }
+}
 
-    for (int i = 0; i < rows_a; i++) {
-        for (int j = 0; j < cols_b; j++) {
-            matrix_c[i][j] = 0;
-            for (int k = 0; k < cols_a; k++) {
-                matrix_c[i][j] += matrix_a[i][k] * matrix_b[k][j];
+// 행렬 곱셈 함수
+void multiplyMatrix(int **a, int **b, int **c, int rows1, int cols1, int cols2) {
+    for(int i=0; i<rows1; i++) {
+        for(int j=0; j<cols2; j++) {
+            c[i][j] = 0;
+            for(int k=0; k<cols1; k++) {
+                c[i][j] += a[i][k] * b[k][j];
             }
         }
     }
+}
 
-    print_matrix(matrix_c, rows_a, cols_b);
-    free_matrix(matrix_c, rows_a, cols_b);
+int main() {
+    int rows1, cols1, rows2, cols2;
+
+    printf("Enter the number of rows of matrix A: ");
+    scanf("%d", &rows1);
+    printf("Enter the number of columns of matrix A: ");
+    scanf("%d", &cols1);
+
+    // 행렬 A 할당
+        int **a = allocateMatrix(rows1, cols1);
+
+    inputMatrix(a, rows1, cols1);
+    printMatrix(a, rows1, cols1);
+
+    printf("\nEnter the number of rows of matrix B: ");
+    scanf("%d", &rows2);
+    printf("Enter the number of columns of matrix B: ");
+    scanf("%d", &cols2);
+
+    // 행렬 B 할당
+    int **b = allocateMatrix(rows2, cols2);
+
+    inputMatrix(b, rows2, cols2);
+    printMatrix(b, rows2, cols2);
+
+    // A와 B의 크기가 다른 경우 프로그램 종료
+    if(rows1 != rows2 || cols1 != cols2) {
+        printf("Error: Matrix sizes do not match\n");
+        freeMatrix(a, rows1);
+        freeMatrix(b, rows2);
+        return 0;
+    }
+
+    // 덧셈 행렬 C 할당
+    int **c = allocateMatrix(rows1, cols1);
+
+    // 덧셈 행렬 계산
+    addMatrix(a, b, c, rows1, cols1);
+    printMatrix(c, rows1, cols1);
+
+    // 뺄셈 행렬 C 할당
+    int **d = allocateMatrix(rows1, cols1);
+
+    // 뺄셈 행렬 계산
+    subtractMatrix(a, b, d, rows1, cols1);
+    printMatrix(d, rows1, cols1);
+
+    // 전치 행렬 T 할당
+    int **t = allocateMatrix(cols1, rows1);
+
+    // 전치 행렬 계산
+    transposeMatrix(a, t, rows1, cols1);
+    printMatrix(t, cols1, rows1);
+
+    // 곱셈 행렬 E 할당
+    int **e = allocateMatrix(rows1, cols2);
+
+    // 곱셈 행렬 계산
+    multiplyMatrix(a, b, e, rows1, cols1, cols2);
+    printMatrix(e, rows1, cols2);
+
+    // 메모리 해제
+    freeMatrix(a, rows1);
+    freeMatrix(b, rows2);
+    freeMatrix(c, rows1);
+    freeMatrix(d, rows1);
+    freeMatrix(t, cols1);
+    freeMatrix(e, rows1);
+
+    return 0;
 }
